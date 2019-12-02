3fe50fe4b878683cc039c2b14718c268eaf8c5af
Alissa <alissabc@hotmail.com>
2019-12-01 19:12:51 -0500

    Updates

---
+++ precache-manifest.b40f751f963589a3ec52c52a51d91369.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ec00b320365bba1be4d037e4972e99e",
+    "url": "/alissaChengQuickNotes/index.html"
+  },
+  {
+    "revision": "453aae3f7f524d1b91ce",
+    "url": "/alissaChengQuickNotes/static/css/main.fef08e20.chunk.css"
+  },
+  {
+    "revision": "6585b47eecbd59b6d33c",
+    "url": "/alissaChengQuickNotes/static/js/2.c9f9d984.chunk.js"
+  },
+  {
+    "revision": "453aae3f7f524d1b91ce",
+    "url": "/alissaChengQuickNotes/static/js/main.da006754.chunk.js"
+  },
+  {
+    "revision": "c5b95027cf623fb60956",
+    "url": "/alissaChengQuickNotes/static/js/runtime-main.01972f80.js"
+  },
+  {
+    "revision": "a71f26e3f7a9b35c145deda90186d921",
+    "url": "/alissaChengQuickNotes/static/media/cork-board-dark.a71f26e3.jpg"
+  }
+]);
